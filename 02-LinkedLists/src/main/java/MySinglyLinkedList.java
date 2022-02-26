f3dd899a7950e6f5cb5dd5d88c67507011660a23
Fatih-Cyber <fatih@cybertekschool.com>
2022-02-26 13:34:22 +0300

    Node Class Added

---
+++ 02-LinkedLists/src/main/java/MySinglyLinkedList.java
@@ -0,0 +1,11 @@
+public class MySinglyLinkedList {
+    Node head;
+    Node tail;
+    int size;
+
+    boolean isEmpty(){};
+    void add(int data){};
+    void printNodes() {};
+    void deleteNode() {};
+    int indexOf(int value){};
+}
