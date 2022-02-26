06ef6e6198d8974beabd1d9f3fd56355493dde90
Fatih-Cyber <fatih@cybertekschool.com>
2022-02-26 20:30:47 +0300

    Iteration added to Node Demo example.

--- 02-LinkedLists/src/main/java/NodeExampleApp.java
+++ 02-LinkedLists/src/main/java/NodeExampleApp.java
@@ -3,9 +3,11 @@ public class NodeExampleApp {
         MyNode nodeA=new MyNode("Node A");
         MyNode nodeB=new MyNode("Node B");
         MyNode nodeC=new MyNode("Node C");
+        MyNode nodeD=new MyNode("Node D");
         nodeA.next=nodeB;
         nodeB.next=nodeC;
-        System.out.println(nodeA.name+" " + nodeA.next.name+" " + nodeB.next.name);
+        nodeC.next=nodeD;
+        //System.out.println(nodeA.name+" " + nodeA.next.name+" " + nodeB.next.name);
 
        // iterate over the list i did
        MyNode current=nodeA;
