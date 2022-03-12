5790c379fb8a033bda8e8951e59cbc646da52e58
Fatih-Cyber <fatih@cybertekschool.com>
2022-03-12 18:54:47 +0300

    Algo Questions Groupped in a different module

--- 15-AlgorithmQuestions/src/main/java/Assignment5SunSetView.java
+++ 15-AlgorithmQuestions/src/main/java/Assignment5SunSetView.java
@@ -1,14 +1,25 @@
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class Assignment5SunSetView {
     public static void main(String[] args) {
         int[] buildings={3, 5, 4, 4, 3, 1, 3, 2};
-        sunsetViews(buildings,"EAST").forEach(System.out::println);
+        sunsetViews(buildings,"WEST").forEach(System.out::println);
     }
     public static ArrayList<Integer> sunsetViews(int[] buildings, String direction) {
-        // Write your code here.
-        // Write your code here.
-        // Write your code here.
-        return new ArrayList<Integer>();
+        Stack<Integer> candidateBuildings=new Stack<>();
+        int startIdx=0;
+        int step=1;
+        int  idx=startIdx;
+        if(direction.equals("WEST")){startIdx= buildings.length-1; step=-1;}
+        while (idx>=0 && idx< buildings.length){
+            while(!candidateBuildings.isEmpty()&& buildings[candidateBuildings.peek()]<=buildings[idx]){
+                candidateBuildings.pop();
+            }
+            candidateBuildings.push(idx);
+            idx+=step;
+        }
+
+        return new ArrayList<Integer>(candidateBuildings);
     }
 }
