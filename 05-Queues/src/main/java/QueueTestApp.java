22094ad887e319aa88e2b6a843967e8d600c7771
Fatih-Cyber <fatih@cybertekschool.com>
2022-03-12 22:41:51 +0300

    Custom Queue class completed

--- 05-Queues/src/main/java/QueueTestApp.java
+++ 05-Queues/src/main/java/QueueTestApp.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 public class QueueTestApp {
     public static void main(String[] args) {
         MyQueue<Integer> myQueue=new MyQueue<>();
@@ -7,5 +9,9 @@ public class QueueTestApp {
         System.out.println(myQueue.peek());
         System.out.println(" Removed item from Queue is :  "+myQueue.dequeue());
         System.out.println(" Removed item from Queue is :  "+myQueue.dequeue());
+        HashMap<Integer, String> a=new HashMap<>();
+        a.put(1, "Sergui");
+        System.out.println(a.containsValue("Sergui"))
+    ;
     }
 }
