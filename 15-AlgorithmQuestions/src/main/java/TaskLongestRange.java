82dff56f1f9c3449e4eb9fe7ce5d9a984d802908
Fatih-Cyber <fatih@cybertekschool.com>
2022-03-12 22:50:15 +0300

    Custom Queue class completed

--- 15-AlgorithmQuestions/src/main/java/TaskLongestRange.java
+++ 15-AlgorithmQuestions/src/main/java/TaskLongestRange.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TaskLongestRange {
     public static void main(String[] args) {
@@ -6,7 +8,30 @@ public class TaskLongestRange {
         System.out.println(Arrays.toString(longestRange(array)));
     }
     public static int[] longestRange(int[]  array){
+    int[] bestRange=new int[2];
+    int longest=0;
+        Map<Integer,Boolean> numsVisited=new HashMap<>();
+        for(int num:array) {
+            numsVisited.put(num,false);
+        }
+        for (int num:array){
+            if(numsVisited.get(num)) continue;
+            numsVisited.put(num,true);
+            int currentLength=1;
+            int left=num-1;
+            int right=num+1;
+            while(numsVisited.containsKey(left)){
+                numsVisited.put(left,true);
+                left--;
+                currentLength++;
+            }
+            while(numsVisited.containsKey(right)){
+                numsVisited.put(right,true);
+                right++;
+                currentLength++;
+            }
 
+        }
     return new int[]{};
     }
 }
