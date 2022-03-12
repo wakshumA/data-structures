1d6cf315cb84174607af3ce9089c23857b1f9675
Fatih-Cyber <fatih@cybertekschool.com>
2022-03-12 20:21:14 +0300

    Reverse First K elements in a Queue added

---
+++ 15-AlgorithmQuestions/src/main/java/TaskLongestRange.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class TaskLongestRange {
+    public static void main(String[] args) {
+        int[] array={1,11,3,0,15,5,2,4,10,7,12,6};
+        System.out.println(Arrays.toString(longestRange(array)));
+    }
+    public static int[] longestRange(int[]  array){
+
+    }
+}
