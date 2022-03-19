fedb2e62eafbb1af5bca1d930b9e3ab3f6915939
Fatih-Cyber <fatih@cybertekschool.com>
2022-03-19 17:56:55 +0300

    Custom Queue class completed

--- 15-AlgorithmQuestions/src/main/java/Assignment6FindFirstNonRepeatingChar.java
+++ 15-AlgorithmQuestions/src/main/java/Assignment6FindFirstNonRepeatingChar.java
@@ -1,10 +1,29 @@
 import org.jetbrains.annotations.NotNull;
 
+import java.sql.SQLOutput;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Assignment6FindFirstNonRepeatingChar {
     public static void main(String[] args) {
-
+        System.out.println(findFirstNonRepeatingChar("a green apple"));
     }
     public static char findFirstNonRepeatingChar(@NotNull String str){
+        Map<Character,Integer> map=new HashMap<>();
+        int count=0;
+        var chars=str.toCharArray();
+        for(Character ch: chars) {
+            if (map.containsKey(ch)) {
+                count=map.get(ch);
+                map.put(ch, count+1);
+            }else {
+                map.put(ch,1);
+            }
 
+        }for(Character ch: chars){
+            if (map.get(ch)==1) return ch;
+        }
+        return Character.MIN_VALUE;
     }
+
 }
