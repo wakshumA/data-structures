78df088f8aada0f640a65bad99e4c5e2215253ab
Fatih-Cyber <fatih@cybertekschool.com>
2022-03-19 21:47:04 +0300

    Algo Question 1 LRU2  cache solution added

--- 15-AlgorithmQuestions/src/main/java/AlgoQuestion1LRUCache2.java
+++ 15-AlgorithmQuestions/src/main/java/AlgoQuestion1LRUCache2.java
@@ -1,2 +1,16 @@
+import java.util.LinkedHashMap;
+
 public class AlgoQuestion1LRUCache2 {
+    public static void main(String[] args) {
+        LRUCache2 lhm=new LRUCache2(3);
+        lhm.put(1,1);
+        lhm.put(2,2);
+        lhm.put(3,3);
+        System.out.println(lhm.get(1));
+
+        lhm.entrySet().forEach(entry->{
+            System.out.println("Key is: "+ entry.getKey() + "Value is:" + entry.getValue());
+        });
+        System.out.println();
+    }
 }
--- 15-AlgorithmQuestions/src/main/java/LRUCache2.java
+++ 15-AlgorithmQuestions/src/main/java/LRUCache2.java
@@ -1,2 +1,19 @@
-public class LRUCache2 {
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LRUCache2 extends LinkedHashMap<Integer,Integer> {
+    private int capacity;
+    public LRUCache2(int capacity){
+        super(capacity, 0.75F, true);
+    }
+public void put(int key,int value){
+        super.put(key,value);
+}
+public int get(int key){return super.get(key);}
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<Integer,Integer> eldest) {
+        return size()>capacity;
+    }
+
 }
