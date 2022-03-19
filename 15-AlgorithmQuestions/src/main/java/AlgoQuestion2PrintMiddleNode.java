2a85883ac014c135c304c3419ddc6c001b62c69c
Fatih-Cyber <fatih@cybertekschool.com>
2022-03-19 22:32:32 +0300

    Algo Question 2 Print Middle added

--- 15-AlgorithmQuestions/src/main/java/AlgoQuestion2PrintMiddleNode.java
+++ 15-AlgorithmQuestions/src/main/java/AlgoQuestion2PrintMiddleNode.java
@@ -1,5 +1,22 @@
 public class AlgoQuestion2PrintMiddleNode {
     public static void main(String[] args) {
-
+    MySinglyLinkedList sll=new MySinglyLinkedList();
+        for (int i = 1; i < 11; i++) {
+            sll.add(i);
+        }
+        sll.printNodes();
+        System.out.println();
+        printMiddle(sll);
+    }
+    public static void printMiddle(MySinglyLinkedList sll){
+        if(sll.isEmpty()) throw new IllegalArgumentException();
+        var sJump=sll.head;
+        var dJump=sll.head;
+        while(dJump!=sll.tail && dJump.next!=sll.tail){
+            sJump=sJump.next;
+            dJump=dJump.next.next;
+        }
+        if(dJump== sll.tail) System.out.println(sJump.value);
+        else System.out.println(sJump.value + "," + sJump.next.value);
     }
 }
