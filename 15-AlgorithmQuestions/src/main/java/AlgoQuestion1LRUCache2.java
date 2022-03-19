2083b74c1aec95620cd182a7bacd0daa6042a7da
Fatih-Cyber <fatih@cybertekschool.com>
2022-03-19 22:08:11 +0300

    Algo Question 2 Print Middle added

--- 15-AlgorithmQuestions/src/main/java/AlgoQuestion1LRUCache2.java
+++ 15-AlgorithmQuestions/src/main/java/AlgoQuestion1LRUCache2.java
@@ -7,7 +7,7 @@ public class AlgoQuestion1LRUCache2 {
         lhm.put(2,2);
         lhm.put(3,3);
         System.out.println(lhm.get(1));
-
+        lhm.put(4,4); // {cache= (4,4), (1,1) , (3,3)}
         lhm.entrySet().forEach(entry->{
             System.out.println("Key is: "+ entry.getKey() + "Value is:" + entry.getValue());
         });
---
+++ 15-AlgorithmQuestions/src/main/java/AlgoQuestion2PrintMiddleNode.java
@@ -0,0 +1,5 @@
+public class AlgoQuestion2PrintMiddleNode {
+    public static void main(String[] args) {
+
+    }
+}
