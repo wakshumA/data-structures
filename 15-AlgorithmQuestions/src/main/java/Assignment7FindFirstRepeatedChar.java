09080cac9157d66a33cd94f75eae434656cfbb8c
Fatih-Cyber <fatih@cybertekschool.com>
2022-03-19 18:19:07 +0300

    Custom Queue class completed

--- 15-AlgorithmQuestions/src/main/java/Assignment7FindFirstRepeatedChar.java
+++ 15-AlgorithmQuestions/src/main/java/Assignment7FindFirstRepeatedChar.java
@@ -1,2 +1,9 @@
 public class Assignment7FindFirstRepeatedChar {
+    public static void main(String[] args) {
+        System.out.println(findFirstRepeatingChar("a green apple"));
+
+    }
+    public static char findFirstRepeatingChar(String str){
+
+    }
 }
