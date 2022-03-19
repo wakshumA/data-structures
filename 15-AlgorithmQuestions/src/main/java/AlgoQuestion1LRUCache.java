b251e49382725503201c83387a817be17a0f8152
Fatih-Cyber <fatih@cybertekschool.com>
2022-03-19 21:04:43 +0300

    Algo Question 1 LRU cache solution added

--- 15-AlgorithmQuestions/src/main/java/AlgoQuestion1LRUCache.java
+++ 15-AlgorithmQuestions/src/main/java/AlgoQuestion1LRUCache.java
@@ -1,5 +1,11 @@
 public class AlgoQuestion1LRUCache {
     public static void main(String[] args) {
-
+        LRUCache lru=new LRUCache(3);
+        lru.put(1, 1);
+        lru.put(2, 2);
+        lru.put(3, 3);
+        System.out.println(lru.get(1));
+        lru.put(4, 4);
+        System.out.println(lru.get(2));
     }
 }
