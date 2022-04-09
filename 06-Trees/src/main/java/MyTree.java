f0b44078cc852578c3bc4ce2ee632daa5e85b3f2
Fatih-Cyber <fatih@cybertekschool.com>
2022-04-09 21:38:54 +0300

    Tree traversals added

--- 06-Trees/src/main/java/MyTree.java
+++ 06-Trees/src/main/java/MyTree.java
@@ -35,4 +35,29 @@ public void insert(int value){
         }
 }// end insert
 
+    // PreOrder Traversal of Tree  Root-Left-Right
+
+    public void preOrderTraversal(Node root){
+        if (root==null) return;
+        System.out.print(root.value+", ");
+        preOrderTraversal(root.leftChild);
+        preOrderTraversal(root.rightChild);
+    }
+    // In-Order Traversal of Tree  Root-Left-Right
+
+    public void inOrderTraversal(Node root){
+        if (root==null) return;
+        inOrderTraversal(root.leftChild);
+        System.out.print(root.value+", ");
+        inOrderTraversal(root.rightChild);
+    }
+// Post-Order traversal  left-right-root
+    public void postOrderTraversal(Node root){
+        if (root==null) return;
+        postOrderTraversal(root.leftChild);
+        postOrderTraversal(root.rightChild);
+        System.out.print(root.value+", ");
+
+    }
+
 }
--- 06-Trees/src/main/java/TreeApp.java
+++ 06-Trees/src/main/java/TreeApp.java
@@ -2,10 +2,17 @@ public class TreeApp {
     public static void main(String[] args) {
         MyTree tree=new MyTree();
         int[] numbers=new int[] {10, 5, 12, 3, 6, 11, 13, 2};
-        for (int i = 0; i <7 ; i++) {
+        for (int i = 0; i <8; i++) {
             tree.insert(numbers[i]);
         }
         VisualizeTree.printTree(tree.root, null,false);
+
+        System.out.println("Pre-Order Traversal");
+        tree.preOrderTraversal(tree.root);
+        System.out.println();
+        System.out.println("In-Order Traversal");
+        tree.inOrderTraversal(tree.root);
+
 
     }
 }
