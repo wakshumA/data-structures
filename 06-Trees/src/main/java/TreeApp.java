dc38caeb44238e52151afa7f8c1433f76e10f5d0
Fatih-Cyber <fatih@cybertekschool.com>
2022-04-09 15:29:38 +0300

    Algo Tree visualisation template added

---
+++ 06-Trees/src/main/java/TreeApp.java
@@ -0,0 +1,9 @@
+public class TreeApp {
+    public static void main(String[] args) {
+
+        int[] numbers=new int[] {10, 5, 12, 3, 6, 11, 13, 2};
+
+        //VisualizeTree.printTree(tree.root, null,false);
+
+    }
+}
