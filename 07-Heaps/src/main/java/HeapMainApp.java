750fd00c2f6f22034b2ad6a0108b16100ad13a40
Fatih-Cyber <fatih@cybertekschool.com>
2022-04-30 22:11:11 +0300

    Heaps insertion and remove added

--- 07-Heaps/src/main/java/HeapMainApp.java
+++ 07-Heaps/src/main/java/HeapMainApp.java
@@ -1,11 +1,14 @@
 public class HeapMainApp {
     public static void main(String[] args) {
-        MyHeap heap=new MyHeap(8);
+        MyHeap heap=new MyHeap(5);
         heap.insert(61);
         heap.insert(120);
         heap.insert(52);
         heap.insert(45);
         heap.insert(83);
         heap.printHeap();
+        System.out.println(heap.remove()+ " is Removed and size is  :"+ heap.size);;
+        heap.printHeap();
+
     }
 }
--- 07-Heaps/src/main/java/MyHeap.java
+++ 07-Heaps/src/main/java/MyHeap.java
@@ -20,6 +20,55 @@ public class MyHeap {
             index=parentIndex(index);
         }
     }
+    public int remove(){
+        if(size==0) throw new IllegalStateException();
+        else{
+            int result=items[0];
+            items[0]=items[--size];
+            bubbleDown();
+            return result;
+        }
+    }
+    public void bubbleDown(){
+        int largerChildIndex;
+        int index=0;
+        while(index<=size && !isValidParent(index)){
+          largerChildIndex=largerChildIndex(index);
+          swap(index, largerChildIndex);
+          index=largerChildIndex;
+
+        }
+    }
+    public int largerChildIndex(int index){
+        if (!hasLeftChild(index)) return index;
+        else if (!hasRightChild(index)){
+            return leftChildIndex(index);
+        }
+        return items[leftChildIndex(index)]>items[rightChildIndex(index)] ? leftChildIndex(index): rightChildIndex(index);
+    }
+    public boolean isValidParent(int index){
+        if(!hasLeftChild(index)) return true;
+        else {
+            boolean isValid =items[index] >= items[leftChildIndex(index)];
+            if (hasRightChild(index)) {
+                isValid &= items[index]>= items[rightChildIndex(index)];
+            }
+            return isValid;
+        }
+
+    }
+    public boolean hasLeftChild(int index){
+        return leftChildIndex(index)<=size;
+    }
+    public boolean hasRightChild(int index){
+        return rightChildIndex(index)<=size;
+    }
+    public int leftChildIndex(int index){
+        return index * 2 + 1;
+    }
+    public int rightChildIndex(int index){
+        return index * 2 + 2;
+    }
     public int parentIndex(int index){
         return (index-1)/2;
     }
@@ -32,5 +81,6 @@ public class MyHeap {
         for (int i = 0; i < size; i++) {
             System.out.print(items[i]+", ");
         }
+        System.out.println();
     }
 }
