dd59c95d92e87247aead44b651315f710803bd4c
Fatih-Cyber <fatih@cybertekschool.com>
2022-05-28 20:59:45 +0300

    TernarySearch added

--- 08-Search/src/main/java/TernarySearch.java
+++ 08-Search/src/main/java/TernarySearch.java
@@ -20,4 +20,23 @@ public static int ternarySearchRecursive(int[] array, int data, int left, int ri
    return ternarySearchRecursive(array, data, middle1+1, middle2-1);
 
 }
+public static int ternarySearchIterative(int[] array, int data){
+        int left=0;
+        int right= array.length-1;
+        while (right>=left){
+            int partition=(right-left)/3;
+            int middle1= left+ partition;
+            int middle2= right+partition;
+
+            if (array[middle1]==data) return middle1;
+            if (array[middle2]==data) return middle2;
+            if(data < array[middle1]) right=middle1-1;
+            else if(data>array[middle2]) left=middle2+1;
+            else {
+                left=middle1+1;
+                right= middle2-1;
+            }
+        }
+     return -1;
+}
 }
