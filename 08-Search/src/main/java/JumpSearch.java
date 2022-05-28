a22d2e7998ff584b3b0c2ae56c28d2653ccb7311
Fatih-Cyber <fatih@cybertekschool.com>
2022-05-28 21:30:59 +0300

    Jump Search template added

--- 08-Search/src/main/java/JumpSearch.java
+++ 08-Search/src/main/java/JumpSearch.java
@@ -1,8 +1,16 @@
 public class JumpSearch {
     public static int jumpSearch(int[] array, int data){
-
-
-
+            int blockSize=(int) Math.sqrt(array.length);
+            int start=0;
+            int next=blockSize;
+            // make jumps in this while loop
+        while(start<array.length && array[next-1]<data) {
+            start=next;
+            next+=blockSize;
+            if (next> array.length) next=array.length;
+        }
+        // now I have found block I will make a linear search
+        for(int i=start; i<next;i++) {if (array[i]==data) return i; }
      return -1;
     }
 }
