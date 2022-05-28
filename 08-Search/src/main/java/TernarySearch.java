4d0d018de19a559e37b1752575d85d598a22b413
Fatih-Cyber <fatih@cybertekschool.com>
2022-05-28 20:51:20 +0300

    Heaps insertion and remove added

--- 08-Search/src/main/java/TernarySearch.java
+++ 08-Search/src/main/java/TernarySearch.java
@@ -1,6 +1,10 @@
 public class TernarySearch {
     public static void main(String[] args) {
-
+        int[] arr=new int[10];
+        for (int i=0;i<10;i++) {
+            arr[i]=i;
+        }
+        System.out.println(ternarySearchRecursive(arr, 4, 0, arr.length-1));
     }
 public static int ternarySearchRecursive(int[] array, int data, int left, int right){
    if (right<left) return -1;//termination
