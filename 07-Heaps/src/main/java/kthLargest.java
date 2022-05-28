170f74cebbc1aedaa788fd9796c78c3bca7c07e3
Fatih-Cyber <fatih@cybertekschool.com>
2022-05-28 16:51:18 +0300

    Heaps insertion and remove added

---
+++ 07-Heaps/src/main/java/kthLargest.java
@@ -0,0 +1,22 @@
+public class kthLargest {
+    public static void main(String[] args) {
+        //int[] numbers={3,2,3,1,2,4,5,5,6};
+        int[] numbers=new int[10];
+        for (int i = 0; i < 10; i++) {
+           numbers[i]=i;
+        }
+
+        System.out.println(getKthLargest(numbers, 4));
+    }
+    public static int getKthLargest(int[] array, int k){
+        // create a Heap for the array and put numbers into the Heap
+        MyHeap heap=new MyHeap(array.length);
+        for (int i = 0; i < array.length ; i++) {
+         heap.insert(array[i]);
+        }
+        for (int i = 0; i < k-1; i++) {
+           heap.remove();
+        }
+        return heap.getRoot();
+    }
+}
