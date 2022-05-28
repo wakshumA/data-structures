8934ef541a33dfdbb1bb9c968e34fe15999e492c
Fatih-Cyber <fatih@cybertekschool.com>
2022-05-28 21:44:24 +0300

    Jump Search template added

--- 08-Search/src/main/java/ExponentialSearch.java
+++ 08-Search/src/main/java/ExponentialSearch.java
@@ -1,2 +1,19 @@
 public class ExponentialSearch {
+    public static void main(String[] args) {
+        int[] arr=new int[10];
+        for (int i=0;i<10;i++) {
+            arr[i]=i;
+        }
+        //System.out.println(ternarySearchRecursive(arr, 4, 0, arr.length-1));
+        System.out.println(exponentialSearch(arr, 4));
+    }
+    public static int exponentialSearch(int[] array, int data){
+        int bound=1;
+        // find proper search window
+        while(bound<array.length && array[bound]<data) bound*=2;
+        int left=bound/2;
+        int right = Math.min(bound, array.length-1);
+        // now call binary search for this window (left ,right)
+        return BinarySearch.binarySearchRecursive(array, data, left, right);
+    }
 }
