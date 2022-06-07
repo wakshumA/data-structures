dad9bf129e289720403f0c2115a97671706ca39e
Fatih-Cyber <fatih@cybertekschool.com>
2022-06-08 02:23:54 +0300

    Search Algo Comparison  added

--- 09-Sorting/src/main/java/BubbleSort.java
+++ 09-Sorting/src/main/java/BubbleSort.java
@@ -1,3 +1,18 @@
 public class BubbleSort {
+    public static int[] bubbleSort( int[] array){
+        for (int i=0; i<array.length; i++) {
+            boolean swap=false;
+            for (int n=0;n<array.length-1-i;n++) {
+                if(array[n]>array[n+1]) { swap (array, n, n+1); swap=true;}
 
+            }
+            if (!swap) return array;
+        }
+        return array;
+    }
+    public static void swap(int[] array, int idx1,int idx2){
+        int temp=array[idx1];
+        array[idx1]=array[idx2];
+        array[idx2]= temp;
+    }
 }
--- 09-Sorting/src/main/java/SortingMainApp.java
+++ 09-Sorting/src/main/java/SortingMainApp.java
@@ -3,6 +3,8 @@ import java.util.Arrays;
 public class SortingMainApp {
     public static void main(String[] args) {
         var myarray=new int[]{8, 2, 4, 1, 3,0,-1};
+        System.out.println(Arrays.toString(BubbleSort.bubbleSort(myarray)));
+
 
     }
 }
